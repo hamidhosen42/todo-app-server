b798f46f903bcb47b5f1eb47d45fd728d256b3a8
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-19 07:44:52 +0600

    final

--- index.js
+++ index.js
@@ -40,7 +40,13 @@ async function run() {
           res.send(result);
         });
 
-
+        // DELETE data from mongodbdb
+        app.delete("/todos/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) };
+          const result = await todoCollection.deleteOne(query);
+          res.send(result);
+        });
   } finally {
   }
 }
